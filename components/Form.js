8d1528a7ea7c61394f6f75e7d9251f42ef967684
timmywimmy <timothykwok95@gmail.com>
2023-04-25 20:32:37 +0800

    data seeding completed

---
+++ components/Form.js
@@ -0,0 +1,13 @@
+import AddUserForm from './addUserForm';
+import UpdateUserForm from './UpdateUserForm';
+
+const Form = () => {
+  const flag = true;
+  return (
+    <div className='container mx-auto py-5'>
+      {flag ? <AddUserForm /> : <UpdateUserForm />}
+    </div>
+  );
+};
+
+export default Form;
